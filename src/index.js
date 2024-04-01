247904ab21f9ab4d7738a9684b2d62dff54a627d
jstramigioli <julianstramigioli@gmail.com>
2024-04-01 20:23:05 -0300

    Added sorting functionality, and refactored some functions to be part of factory functions

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
-import { addNewTask, 
+import {  
         addNewProject, 
         allProjectsArray , 
         deleteTask , 
         deleteProject ,
-        sortTasks
+        
     } from "./todo-logic";
 
 
-const proyPrueba1 = addNewProject(['Proyectito', 'probando el proyectito', 'media', 'ayer',[]])
-const proyPrueba2 = addNewProject(['Proyectito2', 'probando el proyectito2', 'media2', 'ayer',[]])
+const proyPrueba1 = addNewProject(['Proyectito', 'probando el proyectito',[]])
+const proyPrueba2 = addNewProject(['Proyectito2', 'probando el proyectito2',[]])
 
 
-addNewTask(allProjectsArray[0], ['Titulo de prueba', 'esta es una tarea de prueba', 'alta', '5 de mayo'])
-addNewTask(allProjectsArray[0], ['Titulo de prueba2', 'esta es una tarea de prueba2', 'baja', '25 de mayo'])
-addNewTask(allProjectsArray[0], ['Aloha', 'esta es una tarea de prueba2', 'baja', '25 de mayo'])
 
-sortTasks(allProjectsArray[0], 'title')
+/* addNewTask(allProjectsArray[0], ['Titulo de prueba', 'esta es una tarea de prueba', 1, new Date(2024, 5, 11)])
+addNewTask(allProjectsArray[0], ['Titulo de prueba2', 'testa es una tarea de prueba2', 3 , new Date(2024, 3, 3)])
+addNewTask(allProjectsArray[0], ['Aloha', 'zesta es una tarea de prueba2', 2, new Date(2023, 2, 22)])
+
+addNewTask(allProjectsArray[1], ['Titulo de prueba', 'esta es una tarea de prueba', , '5 de mayo'])
+addNewTask(allProjectsArray[1], ['FTitulo de prueba2', 'testa es una tarea de prueba2', 2 , '25 de mayo'])
+addNewTask(allProjectsArray[1], ['Aloha', 'zesta es una tarea de prueba2', 1 , '25 de mayo']) */
+
+
+allProjectsArray[0].addNewTask(['Titulo de prueba', 'esta es una tarea de prueba', 1, new Date(2024, 5, 11)])
+allProjectsArray[0].addNewTask(['Titulo de prueba2', 'testa es una tarea de prueba2', 3 , new Date(2024, 3, 3)])
+allProjectsArray[0].addNewTask(['Aloha', 'zesta es una tarea de prueba2', 2, new Date(2023, 2, 22)])
+
+allProjectsArray[1].addNewTask(['Titulo de prueba', 'esta es una tarea de prueba', , '5 de mayo'])
+allProjectsArray[1].addNewTask(['FTitulo de prueba2', 'testa es una tarea de prueba2', 2 , '25 de mayo'])
+allProjectsArray[1].addNewTask(['Aloha', 'zesta es una tarea de prueba2', 1 , '25 de mayo'])
+
+allProjectsArray[0].sortTasks('dueDate')
+allProjectsArray[1].sortTasks('priority')
+
+console.log(allProjectsArray[0].tasks[0].title.edit('titulo editado'))
 
 console.log(allProjectsArray)
--- src/todo-logic.js
+++ src/todo-logic.js
@@ -1,41 +1,132 @@
+import { isAfter } from "date-fns/isAfter"
+
+// An array with all the projects of the app
+
 const allProjectsArray = []
+
+
+// Properties for task function factory
+
+const hasTitle = (title) => {
+    
+    function sort(project) {
+        project.getTasks().sort((a,b) => a.title.content > b.description.content ? 1 : -1)
+    }
+
+    function edit(newTitle) {
+        this.content = newTitle
+    }
+
+    return {
+        content: title,
+        sort,
+        edit
+    }
+}
+
+const hasDescription = (description) => {
+    function edit(newDescription) {
+        this.content = newDescription
+    }
+    return {
+        content: description,
+        edit
+    }
+}
+
+const hasPriority = (priority) => {
+    function sort(project) {
+        project.getTasks().sort((a,b) => a.priority.value - b.priority.value )
+    }
+    let display
+
+    switch (priority) {
+        case 1: 
+            display = 'Low';
+            break;
+        case 2:
+            display = 'Medium';
+            break;
+        case 3:
+            display = 'High'
+            break;
+        default:
+            display = 'No priority defined' 
+    }
+
+    function edit(newPriority) {
+        this.content = newPriority
+    }
+
+    return {
+        value: priority,
+        display,
+        sort,
+        edit
+    }
+}
+
+const hasDueDate = (dueDate) => {
+
+    function sort(project) {
+        project.getTasks().sort((a,b) => isAfter(a.dueDate.date, b.dueDate.date) ? 1 : -1)
+    }
+
+    function edit(newDueDate) {
+        this.content = newDueDate
+    }
+
+    return {
+        date: dueDate,
+        sort,
+        edit
+    }
+}
+
+// Task Function factory
 
 const createTask = (title, description, priority, dueDate) => {
 
     return {
-        title,
-        description,
-        priority,
-        dueDate
+        title: hasTitle(title),
+        description: hasDescription(description),
+        priority: hasPriority(priority),
+        dueDate: hasDueDate(dueDate)
     }
 }
 
-const createProject = (title, description, priority, dueDate, tasks) => {
+// Project Function Factory
+
+const createProject = (title, description, tasks) => {
+    
     function getTasks()  {return tasks}
+
+    function sortTasks(propertyToSort) {
+        this.tasks.sort(this.tasks[0][propertyToSort].sort(this))
+    }
+
+    function addNewTask(taskProperties) {
+        this.getTasks().push(createTask(...taskProperties))
+    }
+
     return {
         title,
         description,
-        priority,
-        dueDate,
         tasks,
-        getTasks
+        addNewTask,
+        getTasks,
+        sortTasks
     }
 }
 
-const addNewTask = (project, taskProperties) => {
-    project.getTasks().push(createTask(...taskProperties))
-    /*refreshDOM()*/
-}
+//Add new project
 
 const addNewProject = (projectProperties) => {
     allProjectsArray.push(createProject(...projectProperties))
 }
 
-/*const editTask = (task, propertyName, newValue) => {
-    task[propertyName] = newValue
-}
-Creo que esto es al pedo
-*/
+
+// Delete Task
 
 const deleteTask = (task) => {
     for (let i = 0 ; i < allProjectsArray.length ; i++) {
@@ -45,21 +136,18 @@ const deleteTask = (task) => {
     }
 }
 
+
+// Delete Project
+
 const deleteProject = (project) => {
     allProjectsArray.splice(allProjectsArray.indexOf(project), 1)
 }
 
-const sortTasks = (project, criterion) => {
-    switch (criterion) {
-        case 'title' :
-            project.getTasks().sort((a,b) => a > b ? 1 : -1)
-    }
-}
 
-export { addNewTask, 
+export { 
         addNewProject,
         allProjectsArray, 
         deleteTask,
         deleteProject,
-        sortTasks
+        
      }
